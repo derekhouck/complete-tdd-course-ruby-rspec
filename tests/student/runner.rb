dcddbcfd4cff12df054b1b9f4328d2ad2a57f3f3
Derek Houck <derek@derekhouck.com>
2020-09-07 07:31:09 -0700

    Refactor code and use additional tools

--- tests/student/runner.rb
+++ tests/student/runner.rb
@@ -2,4 +2,19 @@ require_relative 'student'
 
 test_student = Student.new('Derek', 'Houck')
 
-puts test_student.first_name
+puts "First name: #{test_student.first_name}"
+puts "Last name: #{test_student.last_name}"
+puts "Full name: #{test_student.student_fullname}"
+puts "Total Enrollment: #{Student.total_count}"
+
+test_student1 = Student.new('John', 'Doe')
+puts "First name: #{test_student1.first_name}"
+puts "Last name: #{test_student1.last_name}"
+puts "Full name: #{test_student1.student_fullname}"
+puts "Total Enrollment: #{Student.total_count}"
+
+test_student2 = Student.new('Jane', 'Doe')
+puts "First name: #{test_student2.first_name}"
+puts "Last name: #{test_student2.last_name}"
+puts "Full name: #{test_student2.student_fullname}"
+puts "Total Enrollment: #{Student.total_count}"
--- tests/student/student_spec.rb
+++ tests/student/student_spec.rb
@@ -1,18 +1,19 @@
 require_relative 'student'
 
 RSpec.describe Student do
-  before do
-    @student = Student.new('John', 'Doe')
-  end
-  it 'should respond to #first_name' do
-    expect(@student).to respond_to(:first_name)
-  end
-
-  it 'should respond to #last_name' do
-    expect(@student).to respond_to(:last_name)
+  describe 'instance methods' do
+    subject { Student.new('John', 'Doe') }
+    it { respond_to(:first_name) }
+    it { respond_to(:last_name) }
+    it { respond_to(:student_fullname) }
   end
 
-  it 'should respond to #student_fullname' do
-    expect(@student).to respond_to(:student_fullname)
+  describe 'total number of students created' do
+    it 'should have two students in total' do
+      Student.new('John', 'Doe')
+      Student.new('Jane', 'Doe')
+
+      expect(Student.total_count).to eq 2
+    end
   end
 end
